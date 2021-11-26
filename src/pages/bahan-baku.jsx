7f7c8755289ec3cc66dc3a7f12f4e8e20ba249b1
hilalhmdy <13519024@std.stei.itb.ac.id>
2021-11-26 09:46:48 +0700

    init

--- src/pages/bahan-baku.jsx
+++ src/pages/bahan-baku.jsx
@@ -4,20 +4,19 @@ import MelihatDaftarData from '../components/MelihatDaftarData';
 import Sidebar from "../components/Sidebar";
 import Container from "../components/Container"
 
-const axios = require('axios').default;
+const axios = require('axios');
 
-const BahanBaku = () => {
+const BahanBaku = () => { 
   const bahan = {
     'Nama Bahan' : [],
     'Banyaknya' : [],
   }
-
-  axios.get('http://localhost:8000/bahan-baku')
-    .then((result) => {
-      console.log(result);
-      bahan['Nama Bahan'].push(result.data[0].name);
-      bahan['Banyaknya'].push(result.data[0].stok);
-    }); 
+  axios.get('http://localhost:5000/bahan-baku')
+  .then((result) => {
+    console.log(result);
+    bahan['Nama Bahan'].push(result.data[0].name);
+    bahan['Banyaknya'].push(result.data[0].stok);
+  }); 
 
   return (
     <Container>
@@ -25,7 +24,7 @@ const BahanBaku = () => {
       <div className='h3 text-center'>Bahan Baku</div>
       <MelihatDaftarData data={bahan} />
     </Container>
-  )
+  );
 }
 
 export default BahanBaku;
